881ed8f7d5896ccedc239d2d8d50c764304cea46
Sean MacLaughlin <seanmaclaughlin@zipcoders-MacBook-Pro.local>
2019-02-06 19:21:20 -0500

    Sum of Input completed

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 /**
- * Created by iyasuwatts on 10/17/17.
+ * @SeanMacLaughlin
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-
+        System.out.println("Give a Number");
+        
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+        int sum = 0;
+        
+        for (int i = 0; i <= input; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
     }
 }
